de4a1c464f0c13521d538b064cba3db8400ef13e
jela3105 <jela3105@gmail.com>
2020-12-24 19:40:39 -0600

    Create UserForm and ViewList componets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserForm from "./components/UserForm";
+import ViewList from "./components/ViewList";
+
+class App extends Component {
+  state = {
+    rout: "list", //form
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ViewList />
+        <UserForm />
+      </div>
+    );
+  }
 }
 
 export default App;
