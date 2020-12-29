a6cc34bde6f091261c7e831093da55e23c770dda
jela3105 <jela3105@gmail.com>
2020-12-28 22:05:35 -0600

    Validate form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,19 +1,54 @@
 import React, { Component } from "react";
+const validate = (values) => {
+  const errors = {};
+  if (!values.name) {
+    errors.name = "The name is required";
+  }
+  if (!values.email) {
+    errors.email = "The name is required";
+  }
+  if (!values.website) {
+    errors.website = "The name is required";
+  }
+  return errors;
+};
 
 export default class UserForm extends Component {
-  state = {};
+  state = {
+    errors: {},
+  };
   handleChange = ({ target }) => {
     this.setState({
       [target.name]: target.value,
     });
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { errors, ...withoutErrors } = this.state;
+    const result = validate(withoutErrors);
+    this.setState({ errors: result });
+    if (!Object.keys(result).length) {
+      //send form
+    }
+    e.target.reset();
+  };
+
   render() {
-    console.log(this.state);
+    const { errors } = this.state;
     return (
-      <form>
-        <input name="name" onChange={this.handleChange} />
-        <input name="email" onChange={this.handleChange} />
-        <input name="website" onChange={this.handleChange} />
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="Name" name="name" onChange={this.handleChange} />
+        {errors.name && <p>{errors.name}</p>}
+        <input placeholder="Email" name="email" onChange={this.handleChange} />
+        {errors.email && <p>{errors.website}</p>}
+        <input
+          placeholder="Web site"
+          name="website"
+          onChange={this.handleChange}
+        />
+        {errors.website && <p>{errors.website}</p>}
+        <input type="submit" value="Send" />
       </form>
     );
   }
